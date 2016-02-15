c7a08c758f395fe4ba38276a989dd84a63ff0441
Chris Wynne <chris.wynndow@gmail.com>
2016-02-15 09:52:20 +0000

    Removes some extra lines of code

--- www/lib/beacons/beacons.js
+++ www/lib/beacons/beacons.js
@@ -6,13 +6,11 @@ beaconsModule.factory('$beaconSniffer', function () {
   var beacons = [];
 
   function onDeviceReady() {
-  $('#found_beacons').text('Device Ready');
   startScan();
   updateTimer = setInterval(checkHotelBeacon, 1000);
   }
 
   function startScan() {
-    $('#found_beacons').text('Start Scan');
 
     function onBeaconsRanged(beaconInfo) {
       for (var i in beaconInfo.beacons) {
@@ -29,19 +27,18 @@ beaconsModule.factory('$beaconSniffer', function () {
       console.log('Ranging beacons did fail: ' + errorMessage);
     }
 
-    $('#found_beacons').text('Before');
     estimote.beacons.requestAlwaysAuthorization();
-    $('#found_beacons').text('After');
     estimote.beacons.startRangingBeaconsInRegion({}, onBeaconsRanged, onError);
+
   }
 
   function checkHotelBeacon() {
     for (var i in beacons) {
       var beacon = beacons[i];
-      if ((beacon.macAddress = 'C5:A5:F9:2D:52:3F') && (beacon.distance < 0.1)) {
+      if ((beacon.macAddress = 'C5:A5:F9:2D:52:3F') && (beacon.distance < 1)) {
         $('#found_beacons').text("You have entered the hotel");
         console.log("Distance: " + beacon.distance + "-In");
-      } else if ((beacon.macAddress = 'C5:A5:F9:2D:52:3F') && (beacon.distance > 0.1)) {
+      } else if ((beacon.macAddress = 'C5:A5:F9:2D:52:3F') && (beacon.distance > 1)) {
         $('#found_beacons').text("You are near the hotel");
         console.log("Distance: " + beacon.distance + '-Near');
       }
