ab3eccf358680dc6ab12cd135b819db52e0cc096
Chris Wynne <chris.wynndow@gmail.com>
2016-02-16 17:13:36 +0000

    Removes duplication from lib and tidy's up login.js

--- www/login/login.js
+++ www/login/login.js
@@ -1,68 +1,64 @@
-
 angular.module('login', ['ionic', 'firebase.utils', 'firebase.auth', 'ngRoute'])
 
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/login', {
-      controller: 'LoginCtrl',
-      templateUrl: 'login/login.html'
-    });
-  }])
-
-  .controller('LoginCtrl', ['$scope', 'Auth', '$location', 'fbutil', function($scope, Auth, $location, fbutil) {
-    $scope.email = null;
-    $scope.pass = null;
-    $scope.confirm = null;
-    $scope.userType = 'guestUser';
-
-    $scope.login = function(email, pass) {
-      $scope.err = null;
-      Auth.$authWithPassword({ email: email, password: pass }, {rememberMe: true})
-        .then(function(/* user */) {
-          console.log('Logged in');
-          $location.path('/beacons');
-        }, function(err) {
-          console.log('failure');
-          $scope.err = errMessage(err);
-        });
-    };
-
-
-    function assertValidAccountProps() {
-      if( !$scope.email ) {
-        $scope.err = 'Please enter an email address';
-      }
-      else if( !$scope.pass || !$scope.confirm ) {
-        $scope.err = 'Please enter a password';
-      }
-      else if( $scope.pass !== $scope.confirm ) {
-        $scope.err = 'Passwords do not match';
-      }
-      return !$scope.err;
-    }
-
-    function errMessage(err) {
-      return angular.isObject(err) && err.code? err.code : err + '';
-    }
-
-  }])
-
-  .run(function($ionicPlatform) {
-    $ionicPlatform.ready(function() {
-      // console.log(estimote);
-      if(window.cordova && window.cordova.plugins.Keyboard) {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-        // Don't remove this line unless you know what you are doing. It stops the viewport
-        // from snapping when text inputs are focused. Ionic handles this internally for
-        // a much nicer keyboard experience.
-        cordova.plugins.Keyboard.disableScroll(true);
-      }
-
-      if(window.StatusBar) {
-        StatusBar.styleDefault();
-      }
-
-    });
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/login', {
+    controller: 'LoginCtrl',
+    templateUrl: 'login/login.html'
   });
+}])
+
+.controller('LoginCtrl', ['$scope', 'Auth', '$location', 'fbutil', function($scope, Auth, $location, fbutil) {
+  $scope.email = null;
+  $scope.pass = null;
+  $scope.confirm = null;
+  $scope.userType = 'guestUser';
+
+  $scope.login = function(email, pass) {
+    $scope.err = null;
+    Auth.$authWithPassword({
+        email: email,
+        password: pass
+      }, {
+        rememberMe: true
+      })
+      .then(function() {
+        console.log('Logged in');
+        $location.path('/beacons');
+      }, function(err) {
+        console.log('failure');
+        $scope.err = errMessage(err);
+      });
+  };
+
+
+  function assertValidAccountProps() {
+    if (!$scope.email) {
+      $scope.err = 'Please enter an email address';
+    } else if (!$scope.pass || !$scope.confirm) {
+      $scope.err = 'Please enter a password';
+    } else if ($scope.pass !== $scope.confirm) {
+      $scope.err = 'Passwords do not match';
+    }
+    return !$scope.err;
+  }
+
+  function errMessage(err) {
+    return angular.isObject(err) && err.code ? err.code : err + '';
+  }
+
+}])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+
+  });
+});
