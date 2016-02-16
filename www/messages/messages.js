a5bffbf580f94fdec731161166ca6fdce1b3a0c5
Chris Wynne <chris.wynndow@gmail.com>
2016-02-16 14:12:47 +0000

    MessageFactor working

--- www/messages/messages.js
+++ www/messages/messages.js
@@ -8,34 +8,33 @@ angular.module('messages', ['ngRoute', 'ngCordova'])
   }])
 
 
-  .controller('messageCtrl', [function () {
+  .controller('messageCtrl', ['messageFactory', function (messageFactory) {
     console.log("Inside the messageCtrl");
 
     self = this;
-    self.message = "hello there";
-    // self.message = messageFactory.getMessage();
+    self.message = messageFactory.getMessage();
     // messageFactory.notifyUser(self.message);
 
-  }]);
+  }])
 
-  // .factory('messageFactory', function($cordovaLocalNotification, $ionicPlatform) {
-  //   return {
-  //     getMessage: function() {
-  //       return "You are staying in room 101";
-  //     },
-  //     notifyUser: function(message) {
-  //       $ionicPlatform.ready(function() {
-  //         $cordovaLocalNotification.schedule({
-  //           id: 1,
-  //           title: 'Warning',
-  //           text: message,
-  //           data: {
-  //             customProperty: 'custom value'
-  //           }
-  //         }).then(function(result) {
-  //           console.log('Notification 1 triggered');
-  //         });
-  //       });
-  //     }
-  //   };
-  // });
+  .factory('messageFactory', function($cordovaLocalNotification, $ionicPlatform) {
+    return {
+      getMessage: function() {
+        return "You are staying in room 101";
+      },
+      notifyUser: function(message) {
+        $ionicPlatform.ready(function() {
+          $cordovaLocalNotification.schedule({
+            id: 1,
+            title: 'Warning',
+            text: message,
+            data: {
+              customProperty: 'custom value'
+            }
+          }).then(function(result) {
+            console.log('Notification 1 triggered');
+          });
+        });
+      }
+    };
+  });
