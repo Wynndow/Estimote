042eb80a9e943f24875d420526fb071076ee8188
Chris Wynne <chris.wynndow@gmail.com>
2016-02-16 17:59:23 +0000

    Removes a lot of unused console.log

--- www/config.js
+++ www/config.js
@@ -1,18 +1,18 @@
 'use strict';
 
-var config =  angular.module('config', [])
+var config = angular.module('config', [])
 
-  .constant('version', '0.1.0')
+.constant('version', '0.1.0')
 
-  .constant('loginRedirectPath', '/login')
+.constant('loginRedirectPath', '/login')
 
-  .constant('FBURL', 'https://hotel-check-in.firebaseio.com')
+.constant('FBURL', 'https://hotel-check-in.firebaseio.com')
 
-  .run(['FBURL', '$timeout', function(FBURL, $timeout) {
-    if( FBURL.match('//INSTANCE.firebaseio.com') ) {
-      angular.element(document.body).html('<h1>Please configure app/config.js before running!</h1>');
-      $timeout(function() {
-        angular.element(document.body).removeClass('hide');
-      }, 250);
-    }
-  }]);
+.run(['FBURL', '$timeout', function(FBURL, $timeout) {
+  if (FBURL.match('//INSTANCE.firebaseio.com')) {
+    angular.element(document.body).html('<h1>Please configure app/config.js before running!</h1>');
+    $timeout(function() {
+      angular.element(document.body).removeClass('hide');
+    }, 250);
+  }
+}]);
