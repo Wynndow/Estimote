a0e5e7ec9b067f8666374c5d116602274aaf4313
edwardkerry <edwardkerry@gmail.com>
2016-02-18 14:31:38 +0000

    Add coveralls

--- karma.conf.js
+++ karma.conf.js
@@ -46,7 +46,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'tests/*.js': ['coverage'],
+      'js/!(app|login)*.js': ['coverage'],
    },
 
 
